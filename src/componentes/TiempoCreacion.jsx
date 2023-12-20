6f2ce9ba44f0a704f6ee47f70692def0363e683a
Martin Rodriguez <martinrodfl@gmail.com>
2023-12-20 18:30:18 -0300

    Agregado Dark-Light mode
    Colores listos

--- src/componentes/TiempoCreacion.jsx
+++ src/componentes/TiempoCreacion.jsx
@@ -2,18 +2,27 @@ import { useEffect, useState } from 'react';
 import { calcularTiempoTranscurrido } from '../funciones/calcularTiempoTranscurrido.js';
 
 const TiempoCreacion = ({ fechaCreacionString }) => {
+	// console.log(
+	// 	'🚀 ~ file: TiempoCreacion.jsx:5 ~ TiempoCreacion ~ fechaCreacionString:',
+	// 	fechaCreacionString
+	// );
+	const fechaCreacion = new Date(fechaCreacionString).toLocaleDateString();
 	const [tiempoTranscurrido, setTiempoTranscurrido] = useState('');
 	useEffect(() => {
 		setInterval(() => {
 			const tiempo = calcularTiempoTranscurrido(fechaCreacionString);
 
 			setTiempoTranscurrido(tiempo);
-		}, 3000);
+		}, 30000);
 	}, [fechaCreacionString]);
 
 	return (
 		<div>
-			<p>Creada hace {tiempoTranscurrido}</p>
+			{tiempoTranscurrido ? (
+				<p>Creada hace {tiempoTranscurrido}</p>
+			) : (
+				<p>Creada el {fechaCreacion}</p>
+			)}
 		</div>
 	);
 };
--- src/funciones/cantidadPorPropiedad.js
+++ src/funciones/cantidadPorPropiedad.js
@@ -1,10 +1,13 @@
+//with tests in /src/test/cantidadPropiedad.test.js
+
 export function cantidadPorPropiedad(arr) {
 	// Verificar si el argumento es un array
 	if (!Array.isArray(arr)) {
 		console.error('Error: El argumento no es un array.');
-		return [];
+		return 0;
 	}
 
 	const completadas = arr.filter((item) => item.completada === true);
+
 	return completadas.length;
 }
