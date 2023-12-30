e4c9c0fffb65a4a287c7b8162881f69795bf86bc
Martin Rodriguez <martinrodfl@gmail.com>
2023-12-29 23:28:54 -0300

    Correcciones en css - posicion de label del input on focus

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-9d4a84ab'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.tgg2aijpctg"
+    "revision": "0.56mo0a13k68"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
